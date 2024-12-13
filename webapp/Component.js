ead689a6e2916477a47bed814fad45f68b0e75f5
Subhashini R <subha.suba@gmail.com>
2024-12-13 15:55:10 +0000

    my first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ns1.prodlist.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
